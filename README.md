282bec87627bd55dcc907820424cbdae77d36308
dralexsand <dr.alex.sand.111@gmail.com>
2025-03-09 23:46:03 +0500

    init

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# random-string-generator
-A simple utility for generating random strings
+<h1 align="center">
+dralexsand/random-string-generator
+</h1>
+
+<p align="center">
+    <strong>A PHP test library package</strong>
+</p>
+
+## About
+
+dralexsand/random-string-generator is a PHP test library package
+
+## Installation
+
+Install this package as a dependency using [Composer](https://getcomposer.org)
+
+``` bash
+composer require dralexsand/random-string-generator
+```
---
+++ src/RandomStringGenerator.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SanPack\RandomStringGenerator;
+
+class RandomStringGenerator
+{
+    public static function generate($length = 10): string
+    {
+        return substr(
+            str_shuffle(
+                str_repeat(
+                    '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+                    $length
+                )
+            ),
+            0,
+            $length
+        );
+    }
+}
